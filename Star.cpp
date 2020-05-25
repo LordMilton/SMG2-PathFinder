e53a63ec578eb509e4e699eacbbcde06cab1c918
Bradley Dufour <dufour.bradley@gmail.com>
2020-05-24 23:48:41 -0400

    Refactor with header

--- Star.cpp
+++ Star.cpp
@@ -1,40 +1,36 @@
-//Represents as an individual level
-class Star
-{	private:
-	String name;
-	int time;
-	int timeWithMedal;
-	
-	public:
-	Star(String name)
-	{
-		this.name = name;
-		time = 600;
-		timeWithMedal = 600;
-	}
-	
-	void setTime(int time)
-	{
-		this.time = time;
-	}
-	
-	void setTimeWithMedal(int time)
-	{
-		timeWithMedal = time;
-	}
-	
-	String getName()
-	{
-		return name;
-	}
-	
-	int getTime()
-	{
-		return time;
-	}
-	
-	int getTimeWithMedal()
-	{
-		return getTimeWithMedal;
-	}
-};
+#include <string>
+#include "Star.h"
+
+using namespace std;
+
+Star::Star(string name)
+{
+	this->name = name;
+	time = 600;
+	timeWithMedal = 600;
+}
+
+void Star::setTime(int time)
+{
+	this->time = time;
+}
+
+void Star::setTimeWithMedal(int time)
+{
+	timeWithMedal = time;
+}
+
+string Star::getName()
+{
+	return name;
+}
+
+int Star::getTime()
+{
+	return time;
+}
+
+int Star::getTimeWithMedal()
+{
+	return timeWithMedal;
+}
