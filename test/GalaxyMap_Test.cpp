350213386efe39cf91668a86e8ae03e7a4c78499
Bradley Dufour <dufour.bradley@gmail.com>
2021-01-28 12:08:51 -0500

    Add pathTime() tests

--- test/GalaxyMap_Test.cpp
+++ test/GalaxyMap_Test.cpp
@@ -40,17 +40,23 @@ TEST_F(GalaxyMap_Test, ContainsNode)
    EXPECT_TRUE(map.containsNode("Test Mid") != NULL);
 }
 
-//TODO Tests pathTime on empty Map
+//Tests pathTime on empty Map
 TEST_F(GalaxyMap_Test, PathTime_Empty)
 {
+   EXPECT_EQ(emptyMap.pathTime("test1", "test2"), -1);
 }
 
 //TODO Tests pathTime with one or both nodes inaccessible
 TEST_F(GalaxyMap_Test, PathTime_Inaccessible)
 {
+   EXPECT_EQ(map.pathTime("none", "Test Begin"), -1);
+   EXPECT_EQ(map.pathTime("Test Begin", "none"), -1);
+   EXPECT_EQ(map.pathTime("none1", "none2"), -1);
 }
 
 //TODO Tests pathTime with valid input
 TEST_F(GalaxyMap_Test, PathTime_Valid)
 {
+   EXPECT_EQ(map.pathTime("Test Begin", "Test End"), MOVE_TIME*3);
+   EXPECT_EQ(map.pathTime("Test End", "Test Begin"), MOVE_TIME*3);
 }
